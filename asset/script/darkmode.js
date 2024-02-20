1eb487b7e79a8e55ae897ec0313540339f1d2ef4
antoine-codeur <antoinejf.denoue@gmail.com>
2024-02-20 10:09:46 +0100

    Suppression des fichiers non-utiles

--- asset/script/darkmode.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('dark-mode-toggle-btn').addEventListener('change', function() {
-    if (this.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.removeAttribute('data-theme');
-    }
-});

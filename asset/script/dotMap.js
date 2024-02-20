b6a9f34e4a5f60470519fc7aa658aeebc1514745
antoine-codeur <antoinejf.denoue@gmail.com>
2024-02-20 10:05:36 +0100

    rendu exo 6

--- asset/script/dotMap.js
+++ asset/script/dotMap.js
@@ -54,29 +54,14 @@ console.log(makeStrings([
 
 // 6) Make an array of the names in h1s, and the ages in h2s
 function readyToPutInTheDOM(arr){
-  // your code here
+  return arr.map(obj => `<h1>${obj.name}</h1><h2>${obj.age}</h2>`);
 }
 console.log(readyToPutInTheDOM([
-    {
-        name: "Angelina Jolie",
-        age: 80
-    },
-    {
-        name: "Eric Jones",
-        age: 2
-    },
-    {
-        name: "Paris Hilton",
-        age: 5
-    },
-    {
-        name: "Kayne West",
-        age: 16
-    },
-    {
-        name: "Bob Ziroll",
-        age: 100
-    }
+    { name: "Angelina Jolie", age: 80 },
+    { name: "Eric Jones", age: 2 },
+    { name: "Paris Hilton", age: 5 },
+    { name: "Kayne West", age: 16 },
+    { name: "Bob Ziroll", age: 100 }
 ])); 
 // ["<h1>Angelina Jolie</h1><h2>80</h2>", 
 // "<h1>Eric Jones</h1><h2>2</h2>", 

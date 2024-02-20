7b900fd95d69842f3e1c3ffcb894e9423c8482d1
antoine-codeur <antoinejf.denoue@gmail.com>
2024-02-20 10:05:05 +0100

    rendu exo 5

--- asset/script/dotMap.js
+++ asset/script/dotMap.js
@@ -36,29 +36,14 @@ console.log(namesOnly([
   
 // 5) Make an array of strings of the names saying whether or not they can go to The Matrix
 function makeStrings(arr){
-    // your code here
+    return arr.map(obj => obj.age >= 18 ? `${obj.name} can go to The Matrix` : `${obj.name} is under age!!`);
 }
 console.log(makeStrings([
-    {
-        name: "Angelina Jolie",
-        age: 80
-    },
-    {
-        name: "Eric Jones",
-        age: 2
-    },
-    {
-        name: "Paris Hilton",
-        age: 5
-    },
-    {
-        name: "Kayne West",
-        age: 16
-    },
-    {
-        name: "Bob Ziroll",
-        age: 100
-    }
+    { name: "Angelina Jolie", age: 80 },
+    { name: "Eric Jones", age: 2 },
+    { name: "Paris Hilton", age: 5 },
+    { name: "Kayne West", age: 16 },
+    { name: "Bob Ziroll", age: 100 }
 ])); 
 // ["Angelina Jolie can go to The Matrix", 
 // "Eric Jones is under age!!", 
